cb22fe4643c529607f2aae94b0e882d7edb469a1
student <stanleyshi42@gmail.com>
2021-10-06 00:48:51 -0700

    Added GET /users/

--- user_api.py
+++ user_api.py
@@ -3,3 +3,9 @@ import sqlite3
 import sqlite_utils
 from sqlite_utils import Database
 import datetime
+
+@hug.get("/users/")
+def retrieve_users():
+    return {"users": db["users"].rows}
+
+db = Database(sqlite3.connect("mockroblog.db"))

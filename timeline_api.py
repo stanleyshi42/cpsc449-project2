3dc00822bc4d446fad1eed2d8a0386d5263bea11
student <stanleyshi42@gmail.com>
2021-10-06 18:42:22 -0700

    GET /user_timeline/ and /public_timeline/ now sorted by timestamp

--- timeline_api.py
+++ timeline_api.py
@@ -4,37 +4,43 @@ import sqlite_utils
 from sqlite_utils import Database
 import requests
 import datetime
+import json
+
+
+db = Database(sqlite3.connect("timelines.db"))
 
 def authenticate_user(username, password):
     """Authenticates a user"""
-    #TODO implement user API
-    #r = requests.get('https://api.github.com/user', auth=(username, password))
-    r = requests.get('http://localhost:8000/users/'+username, auth=(username, password))
-    print(r.json)
-    
+    print('auth called')
+    r = requests.get("http://localhost:8000/users/"+username)
+    user = json.loads(r.text)
+    user = user["users"][0]
+    if user["password"] == password:
+        print('auth passed')
+        return user
+    print('auth failed')
+    return False
 
 @hug.get("/public_timeline/")
 def public_timeline():
-    return {"posts": db["posts"].rows}
+    return {"posts": db["posts"].rows_where(order_by = "timestamp desc")}
 
 @hug.get("/user_timeline/{username}")
 def user_timeline(response, username: hug.types.text):
     posts = []
     try:
-        for row in db["posts"].rows_where("username = ? ORDER BY timestamp DESC", [username]):
-            print(row)
+        for row in db["posts"].rows_where("username = ?", [username], order_by = "timestamp desc"):
             posts.append(row)
     except sqlite_utils.db.NotFoundError:
         response.status = hug.falcon.HTTP_404
     return {"posts": posts}
 
-@hug.get("/home_timeline/{username}")
-def user_timeline(response, username: hug.types.text):
+@hug.get("/home_timeline/{username}", requires=authenticate_user)
+def home_timeline(response, username: hug.types.text):
     #TODO
     posts = []
     try:
         for row in db["posts"].rows_where("username = ? ORDER BY timestamp DESC", [username]):
-            print(row)
             posts.append(row)
     except sqlite_utils.db.NotFoundError:
         response.status = hug.falcon.HTTP_404
@@ -50,12 +56,13 @@ def retrieve_post(response, id: hug.types.number):
         response.status = hug.falcon.HTTP_404
     return {"posts": posts}
 
-@hug.post("/posts/", status=hug.falcon.HTTP_201, requires=authenticate_user) 
+@hug.post("/posts/", status=hug.falcon.HTTP_201, requires=hug.authentication.basic(authenticate_user('stan98', 'p@ssw0rd')))
 def create_post(
     username: hug.types.text,
     text: hug.types.text,
     repost: hug.types.text,
     response,
+    user: hug.directives.user,
 ):
     posts = db["posts"]
 
@@ -75,7 +82,3 @@ def create_post(
 
     response.set_header("Location", f"/posts/{post['id']}")
     return post
-
-db = Database(sqlite3.connect("timelines.db"))
-#authenticate_user("stan98", "p@ssw0rd")
-#authenticate_user("stan98", "lol")

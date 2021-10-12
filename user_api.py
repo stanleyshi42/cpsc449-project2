b7b0044d7f5e72badf732d4527570681341b51ea
student <stanleyshi42@gmail.com>
2021-10-11 19:52:18 -0700

    Added POST /users/{username}/following

--- user_api.py
+++ user_api.py
@@ -8,27 +8,16 @@ db = Database(sqlite3.connect("./var/users.db"))
 
 @hug.get("/users/")
 def retrieve_users():
-    """GETs all users"""
+    """GET all users"""
     return {"users": db["users"].rows}
 
 @hug.get("/users/{username}")
 def retrieve_user(response, username: hug.types.text):
-    """GETs a user by their username"""
+    """GET a user by their username"""
     users = []
     try:
         user = db["users"].get(username)
         users.append(user)
-    except sqlite_utils.db.NotFoundError:
-        response.status = hug.falcon.HTTP_404
-    return {"users": users}
-
-@hug.get("/users/{username}/following")
-def retrieve_following(response, username: hug.types.text):
-    """GETs a user's following list"""
-    users = []
-    try:
-        for row in db["following"].rows_where("follower_id = ?", [username]):
-            users.append(row)
     except sqlite_utils.db.NotFoundError:
         response.status = hug.falcon.HTTP_404
     return {"users": users}
@@ -59,3 +48,36 @@ def create_user(
 
     response.set_header("Location", f"/users/{user['username']}")
     return user
+
+@hug.get("/users/{username}/following")
+def retrieve_following(response, username: hug.types.text):
+    """GET a user's following list"""
+    users = []
+    try:
+        for row in db["following"].rows_where("follower_id = ?", [username]):
+            users.append(row)
+    except sqlite_utils.db.NotFoundError:
+        response.status = hug.falcon.HTTP_404
+    return {"users": users}
+
+@hug.post("/users/{username}/following", status=hug.falcon.HTTP_201)
+def create_following(
+    username: hug.types.text,
+    following_id: hug.types.text,
+    response,
+):
+    """POST a new follower"""
+    follower = {
+        "follower_id": username,
+        "following_id": following_id,
+    }
+
+    try:
+        db["following"].insert(follower)
+        follower["id"] = db["following"].last_pk
+    except Exception as e:
+        response.status = hug.falcon.HTTP_409
+        return {"error": str(e)}
+
+    response.set_header("Location", f"/following/{follower['id']}")
+    return follower
